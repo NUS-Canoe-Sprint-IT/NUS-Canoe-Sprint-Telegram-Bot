07562456f3167e8038b3a7fbd420e0f92f470449
ryanlimjr <ryanlimjr@yahoo.com.sg>
2023-06-02 16:36:53 +0800

    Add Unit test for getWeektoString function, Add documentation to functions in dateUtils.ts

--- Test/dateUtils.test.ts
+++ Test/dateUtils.test.ts
@@ -1,29 +1,29 @@
-import { start } from 'repl';
-import {getFirstDateOfWeek, getLastDateOfWeek, getWeekToString} from '../src/Utils/dateUtils'
+import { start } from "repl";
+import {getFirstDateOfWeek, getLastDateOfWeek, getWeekToString} from "../src/Utils/dateUtils"
 
-describe( 'Method - getFirstDateOfWeek()', () => {
-    test ('Test 1 - date falls on Sunday', () =>{
+describe( "Method - getFirstDateOfWeek()", () => {
+    test ("Test 1 - date falls on Sunday", () =>{
         const date: Date = new Date("2023-05-28"); // Monday falls on 2023-05-22
         const firstDayOfWeek: Date = getFirstDateOfWeek(date);
         const expectedDate: Date = new Date("2023-05-22");
         expect(firstDayOfWeek).toEqual(expectedDate);
     });
 
-    test ('Test 2 - date falls on Monday', () =>{
+    test ("Test 2 - date falls on Monday", () =>{
         const date: Date = new Date("2022-10-17"); // Monday falls on 2022-10-17
         const firstDayOfWeek: Date = getFirstDateOfWeek(date);
         const expectedDate: Date = new Date("2022-10-17");
         expect(firstDayOfWeek).toEqual(expectedDate);
     });
 
-    test ('Test 3.1 - date falls on any other day (Wed)', () =>{
+    test ("Test 3.1 - date falls on any other day (Wed)", () =>{
         const date: Date = new Date("2021-11-17"); // Monday falls on 2021-11-15
         const firstDayOfWeek: Date = getFirstDateOfWeek(date);
         const expectedDate: Date = new Date("2021-11-15");
         expect(firstDayOfWeek).toEqual(expectedDate);
     });
 
-    test ('Test 3.2 - date falls on any other day (Fri)', () =>{
+    test ("Test 3.2 - date falls on any other day (Fri)", () =>{
         const date: Date = new Date("2019-01-18"); // Monday falls on 2021-11-15
         const firstDayOfWeek: Date = getFirstDateOfWeek(date);
         const expectedDate: Date = new Date("2019-01-14");
@@ -32,29 +32,29 @@ describe( 'Method - getFirstDateOfWeek()', () => {
 
 });
 
-describe( 'Method - getLastDateOfWeek()', () => {
-    test ('Test 1 - date falls on Monday', () =>{
+describe( "Method - getLastDateOfWeek()", () => {
+    test ("Test 1 - date falls on Monday", () =>{
         const date: Date = new Date("2023-05-22"); // Sunday falls on 2023-05-28
         const lastDayOfWeek: Date = getLastDateOfWeek(date);
         const expectedDate: Date = new Date("2023-05-28"); 
         expect(lastDayOfWeek).toEqual(expectedDate);
     });
 
-    test ('Test 2 - date falls on Sunday', () =>{
+    test ("Test 2 - date falls on Sunday", () =>{
         const date: Date = new Date("2022-10-16"); // Sunday falls on 2022-10-16
         const firstDayOfWeek: Date = getLastDateOfWeek(date);
         const expectedDate: Date = new Date("2022-10-16");
         expect(firstDayOfWeek).toEqual(expectedDate);
     });
 
-    test ('Test 3.1 - date falls on any other day (Wed)', () =>{
+    test ("Test 3.1 - date falls on any other day (Wed)", () =>{
         const date: Date = new Date("2021-11-17"); // Sunday falls on 2021-11-21
         const firstDayOfWeek: Date = getLastDateOfWeek(date);
         const expectedDate: Date = new Date("2021-11-21");
         expect(firstDayOfWeek).toEqual(expectedDate);
     });
 
-    test ('Test 3.2 - date falls on any other day (Fri)', () =>{
+    test ("Test 3.2 - date falls on any other day (Fri)", () =>{
         const date: Date = new Date("2019-01-18"); // Monday falls on 2021-11-15
         const firstDayOfWeek: Date = getLastDateOfWeek(date);
         const expectedDate: Date = new Date("2019-01-20");
@@ -62,8 +62,8 @@ describe( 'Method - getLastDateOfWeek()', () => {
     });
 });
 
-describe( 'Method - getWeekToString', () => {
-    test ('Test 1 ', () =>{
+describe( "Method - getWeekToString", () => {
+    test ("Test 1 - May 29/05 - Jun 04/06", () =>{
         const startDate: Date = new Date("2023-05-29"); 
         const endDate: Date = new Date("2023-06-04"); 
         const expected: string = "May 29/05 - Jun 04/06";
@@ -71,4 +71,19 @@ describe( 'Method - getWeekToString', () => {
         expect(actual).toEqual(expected);
     });
 
+    test ("Test 2 - Jun 05/06 - Jun 11/06", () =>{
+        const startDate: Date = new Date("2023-06-05"); 
+        const endDate: Date = new Date("2023-06-11"); 
+        const expected: string = "Jun 05/06 - Jun 11/06";
+        const actual: string = getWeekToString(startDate, endDate);
+        expect(actual).toEqual(expected);
+    });
+
+    test ("Test 3 - Mar 27/03 - Apr 02/04", () =>{
+        const startDate: Date = new Date("2023-03-27"); 
+        const endDate: Date = new Date("2023-04-02"); 
+        const expected: string = "Mar 27/03 - Apr 02/04";
+        const actual: string = getWeekToString(startDate, endDate);
+        expect(actual).toEqual(expected);
+    });
 });
--- src/Utils/dateUtils.ts
+++ src/Utils/dateUtils.ts
@@ -1,23 +1,46 @@
 import {months} from "./UtilsConstants";
 
+/**
+ * Computes the date of monday in the week of the supplied date
+ *
+ * @export
+ * @param {Date} d the date of which that week's monday's date is to be computed from  
+ * @return {Date} Monday's date of the week which the supplied date is in
+ */
 export function getFirstDateOfWeek(d: Date): Date {
     var mondayDate: number = d.getDate() - d.getDay() + (d.getDay() == 0 ? -6:1);
     return new Date(d.setDate(mondayDate));
 }
 
+/**
+ * Computes the date of Sunday in the week of the supplied date
+ *
+ * @export
+ * @param {Date} d the date of which that week's monday's date is to be computed from
+ * @return {Date} monday's date of the week which the supplied date is in
+ */
 export function getLastDateOfWeek(d: Date): Date {
     var sundayDate: number = d.getDate() + (d.getDay() == 0 ? 0:(7 - d.getDay()));
     return new Date(d.setDate(sundayDate));
 }
 
+/**
+ * Computes the start date and end date of the week in MMM MM/DD - MMM MM/DD format 
+ * to be used to identify the target sheet based on name
+ *
+ * @export
+ * @param {Date} mondayDate
+ * @param {Date} sundayDate
+ * @return {string} The start date and end date of the week in MMM MM/DD - MMM MM/DD format 
+ */
 export function getWeekToString(mondayDate: Date, sundayDate: Date): string {
-    const startMonthNum: string = ('0' + (mondayDate.getMonth() + 1)).slice(-2);
+    const startMonthNum: string = ("0" + (mondayDate.getMonth() + 1)).slice(-2);
     const startMonth: string = months[mondayDate.getMonth() ];
-    const startDate: string = ('0' + mondayDate.getDate()).slice(-2);
+    const startDate: string = ("0" + mondayDate.getDate()).slice(-2);
 
-    const endMonthNum: string = ('0' + (sundayDate.getMonth() + 1)).slice(-2);
+    const endMonthNum: string = ("0" + (sundayDate.getMonth() + 1)).slice(-2);
     const endMonth: string = months[sundayDate.getMonth()];
-    const endDate: string = ('0' + sundayDate.getDate()).slice(-2);
+    const endDate: string = ("0" + sundayDate.getDate()).slice(-2);
 
     return `${startMonth} ${startDate}/${startMonthNum} - ${endMonth} ${endDate}/${endMonthNum}`;
 }
