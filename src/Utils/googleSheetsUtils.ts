605f6d4d83ac785a1026d5c908977c79f5bf8f81
ryanlimjr <ryanlimjr@yahoo.com.sg>
2023-06-04 12:45:33 +0800

    Add documentation

--- src/Utils/googleSheetsUtils.ts
+++ src/Utils/googleSheetsUtils.ts
@@ -1,3 +1,13 @@
+
+
+/**
+ * Zips the name array and boat array, if a person is not allocated a boat, UNALLOCATED will be tagged to the name
+ *
+ * @export
+ * @param {string[]} name array of names
+ * @param {string[]} boat array of boat allocation
+ * @return {[string, string][]} 
+ */
 export function zipNameAndBoatAlloc(name:string[], boat:string[]): [string, string][] {
     const boatAlloc: [string, string][] = []
     for (let i = 0; i < name.length; i++) {
