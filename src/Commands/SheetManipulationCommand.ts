1aca4f3a80e52db94e2c57f46df0c941e0faeb10
ryanlimjr <ryanlimjr@yahoo.com.sg>
2023-06-07 22:00:01 +0800

    Add return types to functions

--- src/Commands/SheetManipulationCommand.ts
+++ src/Commands/SheetManipulationCommand.ts
@@ -4,6 +4,7 @@ import * as dotenv from "dotenv";
 import * as dateUtils from "../Utils/dateUtils"
 import {zipNameAndBoatAlloc} from "../Utils/googleSheetsUtils"
 import {DATA_RANGE_MORN, DATA_RANGE_AFTN} from "./CommandConstants"
+import { promises } from "dns";
 
 require('dotenv').config();
 
@@ -17,7 +18,7 @@ export class SheetManipulationCommand {
         this.currentWeek = dateUtils.getWeekFromDate(new Date())
     }
 
-    private parseRawWeeklyAttendance(sheetName: string ,data: string[][]) {
+    private parseRawWeeklyAttendance(sheetName: string ,data: string[][]): {[key: string]: [string, string][]} {
         var weeklyAttendance: {[key:string]: [string, string][]} = {}
         for (let i = 0; i < 7; i++) {
             const date: string = data[i * 3][0]
@@ -28,7 +29,7 @@ export class SheetManipulationCommand {
         return weeklyAttendance
     }
 
-    protected async getWeeklyAttendanceOn(date: Date, AM: Boolean) {
+    protected async getWeeklyAttendanceOn(date: Date, AM: Boolean): Promise<{[key: string]: [string, string][]}> {
         const sheetName:string = dateUtils.getWeekFromDate(date)
         const dataRange = AM ? DATA_RANGE_MORN : DATA_RANGE_AFTN
         const sheet = await this.googleSpreadsheetInstance.values.get({   
