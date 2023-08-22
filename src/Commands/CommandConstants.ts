07048c3868bca9dc9b95ab86ff4a09486fc34de2
rouvinerh <rouvinerh@gmail.com>
2023-08-22 05:33:19 -0400

    added broken scenes

--- src/Commands/CommandConstants.ts
+++ src/Commands/CommandConstants.ts
@@ -1,6 +1,12 @@
 export const HELP_MESSAGE = (
     "/help  - Display this message\n" +
     "/getBoatAlloc - display boat allocations\n" +
-    "/form - Fill in daily log sheet. Send your name, contact number, number of 1 star paddlers and non-certified paddlers, start time and end time in 24h format (14:00 for afternoon) like this: \n Rouvin\n 81234567\n 15\n 4\n 07:30\n 09:30\n ONLY NAMES CAN HAVE SPACES")
+    "/form - Fill in daily log sheet. Send your name, contact number, number of 1 star paddlers and non-certified paddlers, start time and end time in 24h format (14:00 for 2pm) without spaces:\n" +
+        "Rouvin\n" + 
+        "81234567\n" +
+        "15\n" +
+        "4\n" +
+        "07:30\n"+
+        "09:30\n")
 export const DATA_RANGE_MORN = "A12:U50"
 export const DATA_RANGE_AFTN = "A53:U92"
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {Context, Telegraf} from 'telegraf';
+import { Context, Telegraf, Scenes, session } from 'telegraf';
 import {botRequest} from 'telegraf/typings/button';
 import {Update} from 'typegram';
 import {google, sheets_v4, Auth, Common, drive_v3} from "googleapis";
@@ -29,24 +29,54 @@ const dispBoatAllocCommand: DispBoatAllocCommand = new DispBoatAllocCommand(goog
 const genMonthlyAttendanceCommand: GenMonthlyAttendanceCommand = new GenMonthlyAttendanceCommand(googleSheetInstance, googleDriveInstance);
 
 /* Initialize Telegram Bot */
-const APIToken: string = (environment == 'prod' ? process.env.PROD_BOT_TOKEN : process.env.TEST_BOT_TOKEN) as string
+const APIToken: string = (environment == 'prod' ? process.env.PROD_BOT_TOKEN : process.env.TEST_BOT_TOKEN) as string;
 const bot: Telegraf<Context<Update>> = new Telegraf(APIToken);
 
+/* Initialise scene for form */
+// does not work due to context issues ;c
+const formScene = new Scenes.BaseScene('form');
+const stage = new Scenes.Stage([formScene]);
+
+formScene.enter((ctx) => {
+    ctx.reply('Enter details (Use /help to see what is required)');
+})
+
+formScene.on("text", (ctx) => {
+    const userInput = ctx.message.text;
+    const values = userInput.split('\n').map(value => value.trim());
+    if (values.length >= 6) {
+        const [name, hp, onestar, zerostar, startTime, endTime] = values;
+        // TestForm.submitForm(name, hp, onestar, zerostar, startTime, endTime);
+        // testing
+        ctx.reply('Form Submitted!');
+        ctx.scene.leave();
+    } else {
+        ctx.reply('Please check your input! Follow the format exactly.');
+    }
+})
+
 /* Initializing all the commands */
+// initialize session + middleware for the scene
+bot.use(session());
+bot.use(stage.middleware());
 bot.start((ctx) => {ctx.reply(greet(ctx.from.first_name));});
 bot.help((ctx) => {ctx.reply(HELP_MESSAGE)})
 bot.command("getBoatAlloc", (ctx) => {dispBoatAllocCommand.getBoatAllocation()
     .then((res) => ctx.reply(res, {parse_mode:"MarkdownV2"}))
     .catch((e) => {console.error(e)})
-})
+});
 bot.command("genAttendance", (ctx) => {genMonthlyAttendanceCommand.generateAttendance()
     .then((res) => ctx.reply(res + " Training attendance created"))
     .catch((e) => {console.error(e)})
-})
+});
+// /form command does not work
+bot.command("form", (ctx) => { 
+    ctx.enter.scene('formScene');
+});
 
-console.log("Launching Telegram bot")
+console.log("Launching Telegram bot");
 bot.launch();
-console.log("Telegram Bot successfully launched")
+console.log("Telegram Bot successfully launched");
 
 /* graceful termination */
 process.once('SIGINT', () => bot.stop('SIGINT'));
