bc1dc3a76af644a207bd32a80ea249d5035858d2
rouvinerh <rouvinerh@gmail.com>
2023-08-25 06:41:06 -0400

    pip > npm

--- src/Commands/TestForm.ts
+++ src/Commands/TestForm.ts
@@ -1,4 +1,7 @@
-import fetch from 'node-fetch';
+import { RequestInfo, RequestInit } from 'node-fetch';
+
+const fetch = (url: RequestInfo, init?: RequestInit) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(url, init));
 
 export class FillForm {
 	public async submitForm(
